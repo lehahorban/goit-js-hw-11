f0877c6c43a291b38b0f864e50d63a7de05a880b
Oleksii Horban <lehagorban93@gmail.com>
2022-06-22 23:24:21 +0300

    async await

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import axios from 'axios';
+// import axios from 'axios';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
