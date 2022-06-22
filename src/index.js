d62448ee8901d06ce6bdb359384547de0784a3b8
Oleksii Horban <lehagorban93@gmail.com>
2022-06-22 22:07:55 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import axios from 'axios';
+// np
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
